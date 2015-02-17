5b7cc72598cf4f5cb18922cbf9255dd5c7a50390
dashamostovskaya <dasha.mostovskaya@aiesec.net>
2015-02-17 07:54:19 +0000

    chek in

--- db/migrate/[ts]_add_password_digest_to_users.rb
+++ db/migrate/[ts]_add_password_digest_to_users.rb
@@ -1,5 +1,6 @@
-class AddPasswordDigestToUsers < ActiveRecord::Migration
+class AddRememberTokenToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :password_digest, :string
+    add_column :users, :remember_token, :string
+    add_index  :users, :remember_token
   end
 end
